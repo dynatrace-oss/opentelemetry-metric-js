377d12357a5e9f10e641bcec12fb0f6e3df115a3
Georg Pirklbauer <georg.pirklbauer@dynatrace.com>
2021-09-28 11:13:58 +0200

    add tests for MetricExporter.export

--- tests/MetricExporter.spec.ts
+++ tests/MetricExporter.spec.ts
@@ -15,6 +15,13 @@
 */
 
 import { DynatraceMetricExporter } from "../src";
+import * as nock from "nock";
+import { MetricKind, MetricRecord, SumAggregator } from "@opentelemetry/metrics";
+import { AggregationTemporality, ValueType } from "@opentelemetry/api-metrics";
+import { ExportResult, ExportResultCode } from "@opentelemetry/core";
+import { Resource } from "@opentelemetry/resources";
+
+
 
 describe("MetricExporter", () => {
 	test("should default to oneagent endpoint", () => {
@@ -40,5 +47,88 @@ describe("MetricExporter", () => {
 		expect(exporter["_reqOpts"].port).toEqual("8080");
 		expect(exporter["_reqOpts"].path).toEqual("/metrics");
 		expect(exporter["_reqOpts"].protocol).toEqual("http:");
+	});
+});
+
+describe("MetricExporter.export", () => {
+	test("should export metrics and return a success message", () => {
+		const target_host =  "https://example.com:8080";
+		const target_path = "/metrics";
+		const target_url = target_host + target_path;
+		const exporter = new DynatraceMetricExporter({
+			url: target_url
+		});
+
+		// if this request is not received with a body matching the regex below,
+		// a non-success error code will be returned, making the expect call below
+		// fail.
+		nock(target_host)
+			.post(target_path, /test,key=value count,delta=10 \d{13}/g)
+			.reply(200);
+
+		const aggregator = new SumAggregator();
+		aggregator.update(10);
+
+		// let metric: UpDownCounterMetric = new UpDownCounterMetric("test", );
+		const rec: MetricRecord = {
+			descriptor: {
+				name: "test",
+				description: "some desc",
+				unit: "unit",
+				metricKind: MetricKind.UP_DOWN_COUNTER,
+				valueType: ValueType.DOUBLE
+			},
+			labels: { "key": "value" },
+			aggregator: aggregator,
+			aggregationTemporality: AggregationTemporality.AGGREGATION_TEMPORALITY_DELTA,
+			resource: Resource.EMPTY,
+			instrumentationLibrary: {
+				name: "my-mock-lib"
+			}
+		};
+
+		exporter.export([rec],
+			(result: ExportResult) => {
+				expect(result.code).toEqual(ExportResultCode.SUCCESS);
+			});
+	});
+
+	test("should return error result when called with invalid metric name", () => {
+		const target_host =  "https://example.com:8080";
+		const target_path = "/metrics";
+		const target_url = target_host + target_path;
+		const exporter = new DynatraceMetricExporter({
+			url: target_url
+		});
+
+		nock(target_host)
+			.post(target_path, /~!@ count,delta=10 \d{13}/g)
+			.reply(400);
+
+		const aggregator = new SumAggregator();
+		aggregator.update(10);
+
+		// let metric: UpDownCounterMetric = new UpDownCounterMetric("test", );
+		const rec: MetricRecord = {
+			descriptor: {
+				name: "~!@",
+				description: "some desc",
+				unit: "unit",
+				metricKind: MetricKind.UP_DOWN_COUNTER,
+				valueType: ValueType.DOUBLE
+			},
+			labels: {},
+			aggregator: aggregator,
+			aggregationTemporality: AggregationTemporality.AGGREGATION_TEMPORALITY_DELTA,
+			resource: Resource.EMPTY,
+			instrumentationLibrary: {
+				name: "my-mock-lib"
+			}
+		};
+
+		exporter.export([rec],
+			(result: ExportResult) => {
+				expect(result.code).toEqual(ExportResultCode.FAILED);
+			});
 	});
 });
